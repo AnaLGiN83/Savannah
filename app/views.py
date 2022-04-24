27155b5d17800904e29359e17542c8f6acb4529b
AnaLGiN83 <zyk83@mail.ru>
2022-04-24 21:48:24 +0300

    MVC Pattern architecture base created

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)  # TODO: Проработать архитектуру для app в корректном месте
+from app import app
+from flask import render_template
 
 
 @app.route('/')
