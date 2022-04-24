b8b65eb972f9ff55b429840686071e4af73ca1a3
AnaLGiN83 <zyk83@mail.ru>
2022-04-25 00:46:00 +0300

    Add alerts and settings empty pages and fix nav bar

--- app/views.py
+++ app/views.py
@@ -5,3 +5,13 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/alerts')
+def alerts():
+    return render_template('alerts.html')
+
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
