32a3302cc3610ce183850243854caf8e22e97bce
AnaLGiN83 <zyk83@mail.ru>
2022-05-17 19:00:01 +0300

    Remove not needed util

--- app/utils.py
+++ app/utils.py
@@ -9,13 +9,6 @@ def tail(path, line_count):
         return ex.returncode, None
 
 
-def tail_jq(path, line_count, jq_command):
-    try:
-        return 0, subprocess.check_output(f"tail -n{line_count} {path} | jq -c '{jq_command}'", shell=True).decode('ascii').split('\n')
-    except subprocess.CalledProcessError as ex:
-        return ex.returncode, None
-
-
 def ctl_status(service_name):
     try:
         return 0, subprocess.check_output(f"systemctl status {service_name} | grep Active", shell=True).decode('ascii')[13:]
