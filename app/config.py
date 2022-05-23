44434521ba48e9997e5e11d35d17b32de7f4e151
AnaLGiN83 <zyk83@mail.ru>
2022-05-23 19:55:07 +0300

    Add port option to config

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 DB_NAME = "savannahData.db"
 HOST = "127.0.0.1"  # 0.0.0.0 for global access
+PORT = 5000
 
 # Flask Secret key. WARNING: Set your own random key here!
 SECRET_KEY = "bf92e84436d835e0acec32029f5790447ad99d4b6464a8b6"
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import app, config
 
 if __name__ == "__main__":
-    app.run(debug=True, host=config.HOST)
+    app.run(debug=True, host=config.HOST, port=config.PORT)
