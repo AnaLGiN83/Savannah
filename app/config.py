9daf8e80b50ab6f995a12a3f6193ca95512b61bd
AnaLGiN83 <zyk83@mail.ru>
2022-05-24 07:29:36 +0300

    Remove old info from config

--- app/config.py
+++ app/config.py
@@ -6,10 +6,10 @@ PORT = 5000
 # Flask Secret key. WARNING: Set your own random key here!
 SECRET_KEY = "bf92e84436d835e0acec32029f5790447ad99d4b6464a8b6"
 
-EVE_PATH = "/var/log/suricata/eve.json"
 SURICATA_LOG_PATH = "/var/log/suricata/suricata.log"
 
 REDIS_HOST = "127.0.0.1"
 REDIS_PORT = 6379
 REDIS_STATS_NAME = "suricata-stats"
 REDIS_ALERTS_NAME = "suricata-alerts"
+
